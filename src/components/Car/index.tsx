66343494b089e761e0127b57c1f40c7a80144fd2
lucasRosssi <lucasrm2305@gmail.com>
2022-02-19 21:28:26 -0300

    worked on car component

--- src/components/Car/index.tsx
+++ src/components/Car/index.tsx
@@ -1,7 +1,59 @@
 import React from 'react';
 
-import { Container } from './styles';
+import GasolineSvg from '../../assets/gasoline.svg';
 
-export function Car() {
-	return <Container></Container>;
+import {
+	Container,
+	Details,
+	Brand,
+	Name,
+	About,
+	Rent,
+	Time,
+	Price,
+	Type,
+	CarImage,
+} from './styles';
+
+interface CarData {
+	brand: string;
+	name: string;
+	rent: {
+		time: string;
+		price: number;
+	};
+	thumbnail: string;
+}
+
+interface CarProps {
+	data: CarData;
+}
+
+export function Car({ data }: CarProps) {
+	return (
+		<Container>
+			<Details>
+				<Brand>{data.brand}</Brand>
+				<Name>{data.name}</Name>
+
+				<About>
+					<Rent>
+						<Time>{data.rent.time}</Time>
+						<Price>R$ {data.rent.price}</Price>
+					</Rent>
+
+					<Type>
+						<GasolineSvg />
+					</Type>
+				</About>
+			</Details>
+
+			<CarImage
+				source={{
+					uri: data.thumbnail,
+				}}
+				resizeMode="contain"
+			/>
+		</Container>
+	);
 }
