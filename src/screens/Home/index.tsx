d1d56156e53f9f45837f1c8d045c3c0304fbf82c
lucasRosssi <lucasrm2305@gmail.com>
2022-02-19 21:28:37 -0300

    added car data and component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -8,6 +8,28 @@ import Logo from '../../assets/logo.svg';
 import { Container, Header, TotalCars } from './styles';
 
 export function Home() {
+	const carData_01 = {
+		brand: 'audi',
+		name: 'RS 5 Coupé',
+		rent: {
+			time: 'ao dia',
+			price: 120,
+		},
+		thumbnail:
+			'https://png.monster/wp-content/uploads/2020/11/2018-audi-rs5-4wd-coupe-angular-front-5039562b.png',
+	};
+
+	const carData_02 = {
+		brand: 'porsche',
+		name: 'Panamera',
+		rent: {
+			time: 'ao dia',
+			price: 340,
+		},
+		thumbnail:
+			'https://pensecarros.com.br/cms/uploads/porsche-panamera-2-9-v6-e-hybrid-4-pdk-60f0ebf583b0a.png',
+	};
+
 	return (
 		<Container>
 			<StatusBar
@@ -25,7 +47,8 @@ export function Home() {
 				<TotalCars>Total de 12 carros</TotalCars>
 			</Header>
 
-			<Car />
+			<Car data={carData_01} />
+			<Car data={carData_02} />
 		</Container>
 	);
 }
