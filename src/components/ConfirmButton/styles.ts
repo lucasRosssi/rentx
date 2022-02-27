495b978bec8c335c6f9c07869bfc174f229f5021
lucasRosssi <lucasrm2305@gmail.com>
2022-02-27 11:53:59 -0300

    created ConfirmButton component

---
+++ src/components/ConfirmButton/styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components/native';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+import { RectButton } from 'react-native-gesture-handler';
+
+export const Container = styled(RectButton)`
+	width: ${RFValue(80)}px;
+	height: ${RFValue(56)}px;
+
+	background-color: ${({ theme }) => theme.colors.shape_dark};
+
+	align-items: center;
+	justify-content: center;
+`;
+
+export const Title = styled.Text`
+	font-family: ${({ theme }) => theme.fonts.primary_500};
+	font-size: ${RFValue(15)}px;
+	color: ${({ theme }) => theme.colors.shape};
+`;
