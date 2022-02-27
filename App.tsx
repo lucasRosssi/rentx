4d05123282b87aec88c74e194a11283c9fbf2842
lucasRosssi <lucasrm2305@gmail.com>
2022-02-27 11:53:44 -0300

    switched initial screen

--- App.tsx
+++ App.tsx
@@ -14,6 +14,7 @@ import theme from './src/styles/theme';
 import { CarDetails } from './src/screens/CarDetails';
 import { Scheduling } from './src/screens/Scheduling';
 import { SchedulingDetails } from './src/screens/SchedulingDetails';
+import { SchedulingComplete } from './src/screens/SchedulingComplete';
 
 export default function App() {
 	const [fontsLoaded] = useFonts({
@@ -30,7 +31,7 @@ export default function App() {
 
 	return (
 		<ThemeProvider theme={theme}>
-			<SchedulingDetails />
+			<SchedulingComplete />
 		</ThemeProvider>
 	);
 }
