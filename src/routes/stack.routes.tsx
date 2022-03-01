9722ad1ccc7ca86ca0bc40fe3a1e0b077150239c
lucasRosssi <lucasrm2305@gmail.com>
2022-03-01 13:26:14 -0300

    changed CarDetails params

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -8,9 +8,11 @@ import { Scheduling } from '../screens/Scheduling';
 import { SchedulingDetails } from '../screens/SchedulingDetails';
 import { SchedulingComplete } from '../screens/SchedulingComplete';
 
+import { CarDTO } from '../dtos/CarDTO';
+
 export type RootStackParamList = {
 	Home: undefined;
-	CarDetails: undefined;
+	CarDetails: { car: CarDTO };
 	Scheduling: undefined;
 	SchedulingDetails: undefined;
 	SchedulingComplete: undefined;
