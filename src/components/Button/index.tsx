01ec721b4f417b3d113af67461e1e5dfa257194a
lucasRosssi <lucasrm2305@gmail.com>
2022-03-03 22:09:13 -0300

    loading indicator in button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,18 +1,33 @@
 import React from 'react';
 import { Pressable, PressableProps } from 'react-native';
+import { useTheme } from 'styled-components';
+import { LoadingIndicator } from '../LoadingIndicator';
 
 import { Container, Title } from './styles';
 
 interface ButtonProps extends PressableProps {
 	title: string;
 	color?: string;
+	isLoading?: boolean;
 }
 
-export function Button({ title, color, disabled, ...rest }: ButtonProps) {
+export function Button({
+	title,
+	color,
+	isLoading,
+	disabled,
+	...rest
+}: ButtonProps) {
+	const theme = useTheme();
+
 	return (
 		<Pressable {...rest}>
 			<Container color={color} enabled={!disabled}>
-				<Title>{title}</Title>
+				{isLoading ? (
+					<LoadingIndicator color={theme.colors.shape} />
+				) : (
+					<Title>{title}</Title>
+				)}
 			</Container>
 		</Pressable>
 	);
