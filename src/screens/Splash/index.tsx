e2d7dcd6b93f4a4b236fe9f2b7faaeeed8f331b2
lucasRosssi <lucasrm2305@gmail.com>
2022-03-05 14:47:34 -0300

    created animated Splash screen

---
+++ src/screens/Splash/index.tsx
@@ -0,0 +1,86 @@
+import React, { useEffect } from 'react';
+import Animated, {
+	useSharedValue,
+	useAnimatedStyle,
+	withTiming,
+	interpolate,
+	Extrapolate,
+	runOnJS,
+} from 'react-native-reanimated';
+
+import { StatusBar, Dimensions } from 'react-native';
+
+import BrandSvg from '../../assets/brand.svg';
+import LogoSvg from '../../assets/logo.svg';
+
+import { Container } from './styles';
+import { useNavigation } from '@react-navigation/native';
+
+const WIDTH = Dimensions.get('window').width;
+
+export function Splash() {
+	const navigation = useNavigation();
+
+	const splashAnimation = useSharedValue(0);
+
+	const brandStyle = useAnimatedStyle(() => {
+		return {
+			opacity: interpolate(splashAnimation.value, [0, 50], [1, 0]),
+			transform: [
+				{
+					translateX: interpolate(
+						splashAnimation.value,
+						[0, 50],
+						[0, -50],
+						Extrapolate.CLAMP
+					),
+				},
+			],
+		};
+	});
+
+	const logoStyle = useAnimatedStyle(() => {
+		return {
+			opacity: interpolate(splashAnimation.value, [0, 25, 50], [0, 0.3, 1]),
+			transform: [
+				{
+					translateX: interpolate(
+						splashAnimation.value,
+						[0, 50],
+						[-50, 0],
+						Extrapolate.CLAMP
+					),
+				},
+			],
+		};
+	});
+
+	function startApp() {
+		navigation.navigate('Home');
+	}
+
+	useEffect(() => {
+		splashAnimation.value = withTiming(50, { duration: 1500 }, () => {
+			'worklet';
+			runOnJS(startApp)();
+		});
+	}, []);
+
+	return (
+		<Container>
+			<StatusBar
+				translucent
+				backgroundColor="transparent"
+				barStyle="light-content"
+			/>
+
+			<Animated.View style={[brandStyle, { position: 'absolute' }]}>
+				<BrandSvg width={80} height={50} />
+			</Animated.View>
+
+			<Animated.View style={[logoStyle, { position: 'absolute' }]}>
+				<LogoSvg width={180} height={20} />
+			</Animated.View>
+		</Container>
+	);
+}
