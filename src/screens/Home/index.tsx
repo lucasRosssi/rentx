7f5916e6aa4b9132bd54b86408155e835018f974
lucasRosssi <lucasrm2305@gmail.com>
2022-03-07 13:30:49 -0300

    worked on floating button animation

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import { StatusBar } from 'react-native';
+import { StatusBar, useWindowDimensions } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 import { useNavigation } from '@react-navigation/native';
 import { useTheme } from 'styled-components';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+import {
+	useSharedValue,
+	useAnimatedStyle,
+	useAnimatedGestureHandler,
+} from 'react-native-reanimated';
+import { PanGestureHandler } from 'react-native-gesture-handler';
 
 import { api } from '../../services/api';
 import { CarDTO } from '../../dtos/CarDTO';
@@ -12,11 +20,54 @@ import Logo from '../../assets/logo.svg';
 import { LoadingIndicator } from '../../components/LoadingIndicator';
 import { Car } from '../../components/Car';
 
-import { CarList, Container, Header, TotalCars, MyCarsButton } from './styles';
+import {
+	CarList,
+	Container,
+	Header,
+	TotalCars,
+	MyCarsButton,
+	MyCarsButtonWrapper,
+} from './styles';
 
 export function Home() {
 	const [cars, setCars] = useState<CarDTO[]>([]);
 	const [loading, setLoading] = useState(true);
+
+	const { width, height } = useWindowDimensions();
+
+	const positionY = useSharedValue(0);
+	const positionX = useSharedValue(0);
+	const myCarsButtonAnimatedStyle = useAnimatedStyle(() => {
+		return {
+			transform: [
+				{ translateX: positionX.value },
+				{ translateY: positionY.value },
+			],
+		};
+	});
+	const onGestureEvent = useAnimatedGestureHandler({
+		onStart(_, ctx: any) {
+			ctx.positionX = positionX.value;
+			ctx.positionY = positionY.value;
+		},
+		onActive(event, ctx: any) {
+			positionX.value = ctx.positionX + event.translationX;
+			positionY.value = ctx.positionY + event.translationY;
+			if (positionX.value > 23) {
+				positionX.value = 23;
+			}
+			if (positionX.value < -width + 83) {
+				positionX.value = -width + 83;
+			}
+			if (positionY.value > 13) {
+				positionY.value = 13;
+			}
+			if (positionY.value < -height + 73) {
+				positionY.value = -height + 73;
+			}
+		},
+		onEnd() {},
+	});
 
 	const theme = useTheme();
 	const navigation = useNavigation();
@@ -69,13 +120,17 @@ export function Home() {
 				/>
 			)}
 
-			<MyCarsButton onPress={handleOpenMyCars}>
-				<Ionicons
-					name="ios-car-sport"
-					size={RFValue(32)}
-					color={theme.colors.shape}
-				/>
-			</MyCarsButton>
+			<PanGestureHandler onGestureEvent={onGestureEvent}>
+				<MyCarsButtonWrapper style={myCarsButtonAnimatedStyle}>
+					<MyCarsButton onPress={handleOpenMyCars}>
+						<Ionicons
+							name="ios-car-sport"
+							size={RFValue(32)}
+							color={theme.colors.shape}
+						/>
+					</MyCarsButton>
+				</MyCarsButtonWrapper>
+			</PanGestureHandler>
 		</Container>
 	);
 }
