61c9c522d565550956a8569d76b1f0da9b29ee2f
lucasRosssi <lucasrm2305@gmail.com>
2022-03-07 10:54:25 -0300

    added home header to splash screen

--- src/screens/Splash/styles.ts
+++ src/screens/Splash/styles.ts
@@ -7,3 +7,19 @@ export const Container = styled.View`
 	align-items: center;
 	justify-content: center;
 `;
+
+export const Header = styled.View`
+	width: 100%;
+	height: 113px;
+
+	position: absolute;
+	top: 0
+
+	background-color: ${({ theme }) => theme.colors.header};
+
+	flex-direction: row;
+	align-items: flex-end;
+	justify-content: space-between;
+
+	padding: 28px 24px;
+`;
