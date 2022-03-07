98f201515ca4701f23e7de1b5a226547af3bbdac
lucasRosssi <lucasrm2305@gmail.com>
2022-03-07 10:52:35 -0300

    log ignore

--- App.tsx
+++ App.tsx
@@ -14,6 +14,12 @@ import theme from './src/styles/theme';
 import { Routes } from './src/routes';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
+import { LogBox } from 'react-native';
+
+LogBox.ignoreLogs([
+	"[react-native-gesture-handler] Seems like you're using an old API with gesture components, check out new Gestures system!",
+]);
+
 export default function App() {
 	const [fontsLoaded] = useFonts({
 		Archivo_400Regular,
