20ceb307d0b03e6cc2184f1ecb46fd6c4f593130
lucasRosssi <lucasrm2305@gmail.com>
2022-03-07 10:54:08 -0300

    worked on splash screen animation

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -7,48 +7,77 @@ import Animated, {
 	Extrapolate,
 	runOnJS,
 } from 'react-native-reanimated';
+import { useNavigation } from '@react-navigation/native';
+import { useTheme } from 'styled-components';
 
 import { StatusBar, Dimensions } from 'react-native';
 
 import BrandSvg from '../../assets/brand.svg';
 import LogoSvg from '../../assets/logo.svg';
 
-import { Container } from './styles';
-import { useNavigation } from '@react-navigation/native';
+import { Container, Header } from './styles';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
-const WIDTH = Dimensions.get('window').width;
+const HEIGHT = Dimensions.get('window').height;
+const statusBarHeight = getStatusBarHeight();
 
 export function Splash() {
 	const navigation = useNavigation();
+	const theme = useTheme();
 
-	const splashAnimation = useSharedValue(0);
+	const brandAnimation = useSharedValue(0);
+	const logoAnimation = useSharedValue(0);
+	const startAppAnimation = useSharedValue(0);
 
 	const brandStyle = useAnimatedStyle(() => {
 		return {
-			opacity: interpolate(splashAnimation.value, [0, 50], [1, 0]),
 			transform: [
 				{
 					translateX: interpolate(
-						splashAnimation.value,
+						brandAnimation.value,
 						[0, 50],
-						[0, -50],
+						[0, 75],
 						Extrapolate.CLAMP
 					),
+				},
+				{
+					scale: interpolate(brandAnimation.value, [0, 50], [1, 0.4]),
+				},
+			],
+			opacity: interpolate(startAppAnimation.value, [0, 1], [1, 0]),
+		};
+	});
+
+	const logoStyle_1 = useAnimatedStyle(() => {
+		return {
+			opacity: interpolate(logoAnimation.value, [0, 25, 50], [0, 0.3, 1]),
+		};
+	});
+
+	const logoStyle_2 = useAnimatedStyle(() => {
+		return {
+			transform: [
+				{
+					scale: interpolate(startAppAnimation.value, [0, 50], [1, 0.6]),
+				},
+				{
+					translateY: interpolate(startAppAnimation.value, [0, 50], [0, -527]),
+				},
+				{
+					translateX: interpolate(startAppAnimation.value, [0, 50], [0, -197]),
 				},
 			],
 		};
 	});
 
-	const logoStyle = useAnimatedStyle(() => {
+	const whiteBackgroundStyle = useAnimatedStyle(() => {
 		return {
-			opacity: interpolate(splashAnimation.value, [0, 25, 50], [0, 0.3, 1]),
 			transform: [
 				{
-					translateX: interpolate(
-						splashAnimation.value,
+					translateY: interpolate(
+						startAppAnimation.value,
 						[0, 50],
-						[-50, 0],
-						Extrapolate.CLAMP
+						[0, 113 - HEIGHT - statusBarHeight]
 					),
 				},
 			],
@@ -60,9 +89,13 @@ export function Splash() {
 	}
 
 	useEffect(() => {
-		splashAnimation.value = withTiming(50, { duration: 1500 }, () => {
-			'worklet';
-			runOnJS(startApp)();
+		brandAnimation.value = withTiming(50, { duration: 800 }, () => {
+			logoAnimation.value = withTiming(50, { duration: 1200 }, () => {
+				startAppAnimation.value = withTiming(50, { duration: 400 }, () => {
+					'worklet';
+					runOnJS(startApp)();
+				});
+			});
 		});
 	}, []);
 
@@ -74,13 +107,31 @@ export function Splash() {
 				barStyle="light-content"
 			/>
 
+			<Header />
+
 			<Animated.View style={[brandStyle, { position: 'absolute' }]}>
 				<BrandSvg width={80} height={50} />
 			</Animated.View>
 
-			<Animated.View style={[logoStyle, { position: 'absolute' }]}>
+			<Animated.View
+				style={[logoStyle_1, logoStyle_2, { position: 'absolute' }]}
+			>
 				<LogoSvg width={180} height={20} />
 			</Animated.View>
+
+			<Animated.View
+				style={[
+					whiteBackgroundStyle,
+					{
+						flex: 1,
+						height: '100%',
+						width: '100%',
+						position: 'absolute',
+						bottom: -HEIGHT - statusBarHeight,
+						backgroundColor: theme.colors.background_primary,
+					},
+				]}
+			/>
 		</Container>
 	);
 }
