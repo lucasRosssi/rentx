9800820579c71e301685a7a704296272b6a41069
lucasRosssi <lucasrm2305@gmail.com>
2022-03-18 13:20:33 -0300

    added new routes

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -3,24 +3,29 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { Splash } from '../screens/Splash';
+import { SignIn } from '../screens/SignIn';
+import { SignUpFirstStep } from '../screens/SignUp/SignUpFirstStep';
+import { SignUpSecondStep } from '../screens/SignUp/SignUpSecondStep';
 import { Home } from '../screens/Home';
 import { CarDetails } from '../screens/CarDetails';
 import { Scheduling } from '../screens/Scheduling';
 import { SchedulingDetails } from '../screens/SchedulingDetails';
-import { SchedulingComplete } from '../screens/SchedulingComplete';
+import { Confirmation, ConfirmationProps } from '../screens/Confirmation';
 import { MyCars } from '../screens/MyCars';
 
 import { CarDTO } from '../dtos/CarDTO';
-import { SignIn } from '../screens/SignIn';
+import { UserDTO } from '../dtos/UserDTO';
 
 export type RootStackParamList = {
 	Splash: undefined;
 	SignIn: undefined;
+	SignUpFirstStep: undefined;
+	SignUpSecondStep: { user: UserDTO };
 	Home: undefined;
 	CarDetails: { car: CarDTO };
 	Scheduling: { car: CarDTO };
 	SchedulingDetails: { car: CarDTO; dates: string[] };
-	SchedulingComplete: undefined;
+	Confirmation: ConfirmationProps;
 	MyCars: undefined;
 };
 
@@ -28,67 +33,42 @@ const { Navigator, Screen } = createNativeStackNavigator<RootStackParamList>();
 
 export function StackRoutes() {
 	return (
-		<Navigator initialRouteName="SignIn">
-			<Screen
-				name="Splash"
-				component={Splash}
-				options={{
-					headerShown: false,
-				}}
-			/>
+		<Navigator initialRouteName="SignIn" screenOptions={{ headerShown: false }}>
+			<Screen name="Splash" component={Splash} />
 			<Screen
 				name="SignIn"
 				component={SignIn}
 				options={{
-					headerShown: false,
-					animation: 'none',
 					gestureEnabled: false,
+				}}
+			/>
+			<Screen
+				name="SignUpFirstStep"
+				component={SignUpFirstStep}
+				options={{
+					animation: 'slide_from_right',
+				}}
+			/>
+			<Screen
+				name="SignUpSecondStep"
+				component={SignUpSecondStep}
+				options={{
+					animation: 'slide_from_right',
 				}}
 			/>
 			<Screen
 				name="Home"
 				component={Home}
 				options={{
-					headerShown: false,
 					animation: 'none',
 					gestureEnabled: false,
 				}}
 			/>
-			<Screen
-				name="CarDetails"
-				component={CarDetails}
-				options={{
-					headerShown: false,
-				}}
-			/>
-			<Screen
-				name="Scheduling"
-				component={Scheduling}
-				options={{
-					headerShown: false,
-				}}
-			/>
-			<Screen
-				name="SchedulingDetails"
-				component={SchedulingDetails}
-				options={{
-					headerShown: false,
-				}}
-			/>
-			<Screen
-				name="SchedulingComplete"
-				component={SchedulingComplete}
-				options={{
-					headerShown: false,
-				}}
-			/>
-			<Screen
-				name="MyCars"
-				component={MyCars}
-				options={{
-					headerShown: false,
-				}}
-			/>
+			<Screen name="CarDetails" component={CarDetails} />
+			<Screen name="Scheduling" component={Scheduling} />
+			<Screen name="SchedulingDetails" component={SchedulingDetails} />
+			<Screen name="Confirmation" component={Confirmation} />
+			<Screen name="MyCars" component={MyCars} />
 		</Navigator>
 	);
 }
