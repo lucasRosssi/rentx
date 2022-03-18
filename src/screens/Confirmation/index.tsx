61b5046c29e3d0a2b3d611e061f2fdae5d495dbd
lucasRosssi <lucasrm2305@gmail.com>
2022-03-18 13:20:09 -0300

    renamed screen and applied route params

--- src/screens/Confirmation/index.tsx
+++ src/screens/Confirmation/index.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import { StatusBar, useWindowDimensions } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 
 import LogoSvg from '../../assets/logo_background_gray.svg';
 import DoneSvg from '../../assets/done.svg';
 import { ConfirmButton } from '../../components/ConfirmButton';
 
 import { Container, Content, Title, Message, Footer } from './styles';
+import { RootStackParamList } from '../../routes/stack.routes';
 
-export function SchedulingComplete() {
+export interface ConfirmationProps {
+	title: string;
+	message: string;
+	nextNavigation: keyof RootStackParamList;
+}
+
+export function Confirmation() {
 	const { width } = useWindowDimensions();
 	const navigation = useNavigation();
+	const route = useRoute();
+	const { title, message, nextNavigation } = route.params as ConfirmationProps;
 
 	function handleConfirm() {
-		navigation.navigate('Home');
+		navigation.navigate(nextNavigation);
 	}
 
 	return (
@@ -27,13 +36,9 @@ export function SchedulingComplete() {
 
 			<Content>
 				<DoneSvg width={80} height={80} />
-				<Title>Carro alugado!</Title>
+				<Title>{title}</Title>
 
-				<Message>
-					Agora você só precisa ir{'\n'}
-					até a concessionária da RENTX{'\n'}
-					pegar o seu automóvel
-				</Message>
+				<Message>{message}</Message>
 			</Content>
 
 			<Footer>
