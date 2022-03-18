c2f5e1be8ea1bc93bcba7e24b636fb458030ad0b
lucasRosssi <lucasrm2305@gmail.com>
2022-03-18 13:20:22 -0300

    fixed screen name in navigation

--- src/screens/SchedulingDetails/index.tsx
+++ src/screens/SchedulingDetails/index.tsx
@@ -93,7 +93,13 @@ export function SchedulingDetails() {
 					id: car.id,
 					unavailable_dates,
 				})
-				.then(() => navigation.navigate('SchedulingComplete'))
+				.then(() =>
+					navigation.navigate('Confirmation', {
+						title: 'Carro alugado!',
+						message: `Agora você só precisa ir\naté a concessionária da RENTX\npegar o seu automóvel`,
+						nextNavigation: 'Home',
+					})
+				)
 				.catch(() => {
 					setLoading(false);
 					Alert.alert('Não foi possível confirmar o agendamento');
