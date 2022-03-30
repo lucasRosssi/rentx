1d4d447386e2a46e324656301f5d64b5f6fe702e
lucasRosssi <lucasrm2305@gmail.com>
2022-03-30 18:50:00 -0300

    fixed disabled attr

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -22,7 +22,7 @@ export function Button({
 	const theme = useTheme();
 
 	return (
-		<Pressable {...rest}>
+		<Pressable disabled={disabled} {...rest}>
 			<Container color={color} enabled={!disabled}>
 				{isLoading ? (
 					<ActivityIndicator color={theme.colors.shape} />
