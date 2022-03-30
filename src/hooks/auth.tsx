f4a72ba45b4fea2dfbe0f1ced6351c0ad2faaff4
lucasRosssi <lucasrm2305@gmail.com>
2022-03-30 18:50:41 -0300

    fixed splash trying to navigate to SignIn

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -30,6 +30,7 @@ interface AuthContextData {
 	signIn: (credentials: SignInCredentials) => Promise<void>;
 	signOut: () => Promise<void>;
 	updateUser: (user: User) => Promise<void>;
+	isLoading: boolean;
 }
 
 interface AuthProviderProps {
@@ -40,6 +41,7 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps) {
 	const [data, setData] = useState<User>({} as User);
+	const [isLoading, setIsLoading] = useState(true);
 
 	async function signIn({ email, password }: SignInCredentials) {
 		try {
@@ -113,6 +115,7 @@ function AuthProvider({ children }: AuthProviderProps) {
 					'Authorization'
 				] = `Bearer ${userData.token}`;
 				setData(userData);
+				setIsLoading(false);
 			}
 		}
 
@@ -126,6 +129,7 @@ function AuthProvider({ children }: AuthProviderProps) {
 				signIn,
 				signOut,
 				updateUser,
+				isLoading,
 			}}
 		>
 			{children}
